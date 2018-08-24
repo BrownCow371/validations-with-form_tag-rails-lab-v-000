a16592fc3abf466268f02655f3e1f2e659d78ffe
Jamie Brown <jmbrowncow@gmail.com>
2018-08-24 20:13:02 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
 
   def show
   end
-  
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category)
+  end
+
 end
