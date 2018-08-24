99e4996b08b8df374a181bb9a4a39afd95f2721c
Jamie Brown <jmbrowncow@gmail.com>
2018-08-24 20:14:55 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,7 @@
 class AuthorsController < ApplicationController
   before action :set_authot, only: [:show, :edit, :update]
   def show
-    @author = Author.find(params[:id])
-  end
+    end
 
   def new
     @author = Author.new
@@ -18,5 +17,11 @@ class AuthorsController < ApplicationController
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  private
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
