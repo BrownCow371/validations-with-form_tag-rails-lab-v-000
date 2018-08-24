4e829595dc105b1af57f015a7a85f0c968863379
Jamie Brown <jmbrowncow@gmail.com>
2018-08-24 20:15:22 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,12 @@ class PostsController < ApplicationController
   def update
   end
 
+private
   def post_params
     params.permit(:title, :content, :category)
   end
 
+  def set_post
+    @author = Author.find(params[:id])
+  end
 end
